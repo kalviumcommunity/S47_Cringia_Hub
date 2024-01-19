2afa680d263c0988622b9ca5884969d6e148e032
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2024-01-19 09:32:43 +0530

    practiced working with express and nodemon

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+//routes
+
+app.get('/ping', (req, res) => {
+    res.send("Hello devoob");
+})
+
+app.listen(4000, ()=> {
+    console.log("Listening on port 4000");
+})
+
