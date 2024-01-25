1d41218a08d9b5caf20be49175aadc6134d6ba33
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2024-01-25 15:48:03 +0530

    ASAP project done

--- user.js
+++ user.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose')
 
-// A singleton to ensure we only start the database once
-// assign the MongoMemoryServer instance to mongoServer
 let mongoServer
 const uri =
 	'mongodb+srv://devraj326:dev326min@cluster0.h7stzr1.mongodb.net/?retryWrites=true&w=majority'
 
 const startDatabase = async () => {
-	// Your code here
+
 	await mongoose.connect(uri)
   console.log('connected to database')
 }
 
 const stopDatabase = async () => {
-	// Your code here
+
 	mongoose.disconnect(uri)
 }
 
